d95151bf02baf388a78052c40d2ad41fa9cdb2ce
Karlo Verde <karlo.verde.1996@gmail.com>
2022-02-08 01:56:37 -0500

    feat: use pre-defined app navigator

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,53 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_navigation_packages/routers/interface.dart';
+import 'package:flutter_navigation_packages/state/book_details.notifier.dart';
+import 'package:flutter_navigation_packages/state/books.notifier.dart';
+import 'package:provider/provider.dart';
+
+Future<void> startApp({
+  required AppNavigator navigator,
+}) async =>
+    runZonedGuarded(
+      () async {
+        WidgetsFlutterBinding.ensureInitialized();
+        FlutterError.onError = (details) => log(
+              details.exceptionAsString(),
+              error: details.exception,
+              stackTrace: details.stack,
+            );
+        runApp(
+          Provider<AppNavigator>.value(
+            value: navigator,
+            child: const NavigationApp(),
+          ),
+        );
+      },
+      (error, stackTrace) => log(
+        error.toString(),
+        error: error,
+        stackTrace: stackTrace,
+      ),
+    );
 
 class NavigationApp extends StatelessWidget {
   const NavigationApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        title: 'Navigation App',
-        home: Scaffold(
-          body: SafeArea(
-            child: Center(
-              child: FractionallySizedBox(
-                widthFactor: 0.5,
-                child: FittedBox(
-                  child: Text(
-                    'Navigation App',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
+  Widget build(BuildContext context) {
+    final navigator = context.watch<AppNavigator>();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BooksNotifier()),
+        ChangeNotifierProvider(create: (_) => BookDetailsNotifier()),
+      ],
+      child: MaterialApp.router(
+        title: navigator.name,
+        routerDelegate: navigator.routerDelegate,
+        routeInformationParser: navigator.routeInformationParser,
+      ),
+    );
+  }
 }
