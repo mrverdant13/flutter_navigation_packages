f00f6a90df11e3f4224afaf6ebf96bc84a43c964
Karlo Verde <karlo.verde.1996@gmail.com>
2022-02-08 00:30:38 -0500

    feat: create book entity

---
+++ lib/models/book.entity.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+@immutable
+class Book extends Equatable {
+  const Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.imageUrl,
+  });
+
+  factory Book.fakeFromId(int id) => Book(
+        id: id,
+        title: 'Title Of The Book #$id',
+        author: 'Author $id',
+        imageUrl: 'https://picsum.photos/id/$id/300/400',
+      );
+
+  final int id;
+  final String title;
+  final String author;
+  final String imageUrl;
+
+  @override
+  List<Object> get props => [id, title, author, imageUrl];
+}
