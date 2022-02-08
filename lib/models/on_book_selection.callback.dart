8a6692838380847ba50e0548629e6f95cf6c552a
Karlo Verde <karlo.verde.1996@gmail.com>
2022-02-08 00:51:24 -0500

    feat: create book selection callback

---
+++ lib/models/on_book_selection.callback.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_navigation_packages/models/book.entity.dart';
+
+typedef OnBookSelectionCallback = void Function(
+  BuildContext context,
+  Book book,
+);
