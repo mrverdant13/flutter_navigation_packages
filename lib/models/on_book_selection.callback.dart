338104373a13ebcee1194968edd058ee5e23cab9
Karlo Verde <karlo.verde.1996@gmail.com>
2022-02-08 04:31:41 -0500

    chore: drop unused typedef

--- lib/models/on_book_selection.callback.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_navigation_packages/models/book.entity.dart';
-
-typedef OnBookSelectionCallback = void Function(
-  BuildContext context,
-  Book book,
-);
