24e8f639a411edaef01483767285f113198f7596
Karlo Verde <karlo.verde.1996@gmail.com>
2022-02-08 00:36:36 -0500

    feat: create books reloading button

---
+++ lib/widgets/reload_books_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation_packages/state/books.notifier.dart';
+import 'package:provider/provider.dart';
+
+class ReloadBooksButton extends StatelessWidget {
+  const ReloadBooksButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => IconButton(
+        onPressed: () => context.read<BooksNotifier>().loadBooks(),
+        icon: const Icon(Icons.refresh),
+      );
+}
